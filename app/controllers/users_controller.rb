acf259155c1aa97d4b031fe79cc9f58942c1a060
ds041202 <ds041202@yahoo.co.jp>
2016-02-19 17:56:55 +0000

    complete work2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
+                                        :following, :followers]
   
   def show
    @user = User.find(params[:id])
@@ -26,10 +28,39 @@ class UsersController < ApplicationController
     end
   end
     
+  # def following  ←　メソッド名間違っていました。ルーティングを参照。
+  #   @title = "Following"
+  #   @user  = User.find(params[:id])
+  #   @users = @user.following.paginate(page: params[:page])　　←リレーションのモデル名間違っていました。userモデル参照。
+  #   render 'show_follow'　　　　　　　　　　↑.pagenateというメソッドはgemが必要なので、現段階では使えません。（テキストには出てこない）
+  # end
+
+  # def followers
+  #   @title = "Followers"
+  #   @user  = User.find(params[:id])
+  #   @users = @user.followers.paginate(page: params[:page])
+  #   render 'show_follow'
+  # end
+  
+  # 以下が修正後のもの（比較して下さい）
+  
+  def followings
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following_users
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.follower_users
+    render 'show_follow'
+  end
+    
   def update
     @user = User.find(params[:id])
-    if @user.update_attributes(user_params) and
-      current_user == @user
+    if current_user == @user && @user.update_attributes(user_params)
       redirect_to @user #user#showへリダイレクトする処理です。TOPページへリダイレクトするときはredirect_to root_url
     else 
       redirect_to edit_user_path(current_user)
